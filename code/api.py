dbe42e66f64da9ffd8b6a3bc3d83fa641cb3c3e4
Leonid A. Movsesjan <lmovsesjan@gmail.com>
2012-09-29 18:52:17 +0400

    fixed code

--- code/api.py
+++ code/api.py
@@ -9,38 +9,45 @@ from cocaine.decorators import http
 
 import requests
 
+def error(code):
+	return code, (), 'Bad request'
+
 def factor(width, height, old_width, old_height):
-    if 0 == width:
-        return float(height) / float(old_height)
-    if 0 == height:
-        return float(width) / float(old_width)
+	if 0 == width:
+		return height / old_height
+	if 0 == height:
+		return width / old_width
 
-    f1 = float(old_height) / float(old_width);
-    f2 = float(height) / float(width);
-
-    if (f1 < f2):
-        return float(height) / float(old_height)
-    else:
-        return float(width) / float(old_width)
+	if (old_height / old_width < height / width):
+		return height / old_height
+	else:
+		return width / old_width
 
 @http
 def resize(meta, request):
-    url = request['url']
+	try:
+		url = request['url']
 
-    width = request['width']
-    height = request['height']
+		width = request['width']
+		height = request['height']
+	except KeyError:
+		return error(400)
 
-    r = requests.get(url, timeout = 2)
+	try:
+		r = requests.get(url, timeout = 2)
+	except:
+		return error(404)
 
-    img = Image(Blob(r.content))
+	img = Image(Blob(r.content))
 
-    f = factor(width, height, img.columns(), img.rows())
+	args = [float(arg) for arg in (width, height, img.columns(), img.rows())]
+	f = factor(*args)
 
-    s = "!%sx%s" % (float(img.columns()) * float(f) + 0.5, float(img.rows()) * float(f) + 0.5)
+	s = '!%sx%s' % (img.columns() * f + 0.5, img.rows() * f + 0.5)
 
-    img.sample(s)
+	img.sample(s)
 
-    blob = Blob()
-    img.write(blob, "jpg")
+	blob = Blob()
+	img.write(blob, 'jpg')
 
-    return 200, (('Content-Type', 'image/jpg'), ('Content-Length', str(len(blob.data)))), str(blob.data)
+	return 200, (('Content-Type', 'image/jpg'), ('Content-Length', str(len(blob.data)))), str(blob.data)
